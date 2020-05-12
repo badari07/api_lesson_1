5b3f7ffe54876a814885e8757f93c271196dfd13
badari07 <badari0793@gmail.com>
2020-05-13 01:06:33 +0530

    feat: adding server & fix package, insomnia

--- src/server.js
+++ src/server.js
@@ -1,15 +1,28 @@
-import express from 'express'
-import { json, urlencoded } from 'body-parser'
-import morgan from 'morgan'
-import cors from 'cors'
+import express from "express";
+import { json, urlencoded } from "body-parser";
+import morgan from "morgan";
+import cors from "cors";
 
-export const app = express()
+export const app = express();
 
-app.disable('x-powered-by')
+app.disable("x-powered-by");
 
-app.use(cors())
-app.use(json())
-app.use(urlencoded({ extended: true }))
-app.use(morgan('dev'))
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
-export const start = () => {}
+app.get("/", (req, res) => {
+  res.send({ message: "hello" });
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send({ message: "ok" });
+});
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log("server is running on port 3000");
+  });
+};
