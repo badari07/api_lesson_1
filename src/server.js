4b9331797a68c314437b9de3ec0332bae3b58e40
badari07 <badari0793@gmail.com>
2020-05-13 21:17:49 +0530

    feat: learnt CRUD op

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ import morgan from "morgan";
 import cors from "cors";
 
 export const app = express();
+//sub-routing
+const router = express.Router();
 
 app.disable("x-powered-by");
 
@@ -12,13 +14,38 @@ app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
-app.get("/", (req, res) => {
+//coustem middleware
+const log = (req, res, next) => {
+  console.log("logging");
+  next(); // this use to call next middleware or controler
+};
+
+//to use on everything
+//app.use(log);
+
+router.get("/me", (req, res) => {
+  res.send({ message: "oh oh" });
+});
+
+//telling use /api/me for router
+app.use("/api", router);
+
+app.get("/", log, (req, res) => {
   res.send({ message: "hello" });
 });
 
 app.post("/", (req, res) => {
   console.log(req.body);
   res.send({ message: "ok" });
+});
+
+app.get("/data", (req, res) => {
+  res.send({ data: [1, 2, 3] });
+});
+
+app.post("/data", (req, res) => {
+  console.log(req.body);
+  res.send({ ok: true });
 });
 
 export const start = () => {
